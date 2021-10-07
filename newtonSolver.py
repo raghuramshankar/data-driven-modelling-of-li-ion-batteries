8e89aa0090d4233736e267f319eaf1fbab1575df
Darkside <raghuram.shankar98@gmail.com>
2021-10-07 07:10:25 +0200

    implement jit compilation

--- newtonSolver.py
+++ newtonSolver.py
@@ -1,4 +1,5 @@
 import jax as jax
+from jax import jit
 import jax.numpy as jnp
 import time
 # import matplotlib.pyplot as plt
@@ -6,22 +7,27 @@ import time
 
 jax.config.update('jax_platform_name', 'cpu')
 
-def func(x):
+@jit
+def func(x0):
     # return jnp.sin(x) + jnp.cos(y)
-    return jnp.cos(x)
+    return jnp.cos(x0)
+
+@jit
+def loss(x0, jac = 1, hes = 1):
+    jac = jax.grad(func)(x0)
+    hes = jax.grad(jax.grad(func))(x0)
+    delX = -jac/hes
+    return jac, hes, delX
 
 if __name__ == '__main__':
     start = time.time()
     x0 = jnp.pi/5
     # y0 = 0
     jac = 1
-    hes = 1
+    # hes = 1
     t = 0.01
-
-    while abs(jac) >= 1e-3:
-        jac = jax.grad(func)(x0)
-        hes = jax.grad(jax.grad(func))(x0)
-        delX = -jac/hes
+    while abs(jac) >= 1e-7:
+        jac, hes, delX = loss(x0)
         x0 = x0 + t * delX
         # print('jac = ', jac)
         # print('x0 in deg = ', x0 * 180/jnp.pi)
