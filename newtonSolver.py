5c46ed595fe84da75ad9c0143c07e17d382b4bef
Darkside <raghuram.shankar98@gmail.com>
2021-10-10 11:40:21 +0200

    make fixed number of iterations

--- newtonSolver.py
+++ newtonSolver.py
@@ -10,7 +10,9 @@ jax.config.update('jax_platform_name', 'gpu')
 @jit
 def func(x0):
     # return jnp.sin(x) + jnp.cos(y)
-    return jnp.cos(x0)
+    # return jnp.cos(x0)
+    # return jnp.sqrt(jnp.tan(x0) * jnp.square(x0)/jnp.cos(x0*jnp.sqrt(x0)))
+    return jnp.sqrt(100 - jnp.square(x0)) + 0
 
 @jit
 def loss(x0, jac = 1, hes = 1):
@@ -18,19 +20,24 @@ def loss(x0, jac = 1, hes = 1):
     hes = jax.grad(jax.grad(func))(x0)
     delX = -jac/hes
     x0 = x0 + t * delX
-    return jac, x0
+    y = func(x0)
+    return jac, x0, y
 
 if __name__ == '__main__':
     start = time.time()
-    x0 = jnp.pi/5
+    # x0 = jnp.pi/5
+    x0 = 9.0
     # y0 = 0
-    jac = 1
+    jac = 1.0
     # hes = 1
-    t = 0.01
-    while abs(jac) >= 1e-7:
-        jac, x0 = loss(x0)
-        # print('jac = ', jac)
+    t = 1.0
+    for _ in range(5):
+        jac, x0, y = loss(x0)
+        print('jac = ', jac)
         # print('x0 in deg = ', x0 * 180/jnp.pi)
+        print('x0 = ', x0)
+        print('y = ', y)
+        print('\n')
 
     # print(func(jnp.pi/2, jnp.pi/2))
     # print(jax.grad(func)(jnp.pi/2, jnp.pi/2))
